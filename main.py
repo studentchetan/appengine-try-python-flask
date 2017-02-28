2d3a8e10514903d536ea1a83f96108bc50b9b3a6
studentchetan <chetankar65@gmail.com>
2017-02-28 17:18:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World!This is chetans first software!I am so excited!'
 
 
 @app.errorhandler(404)
